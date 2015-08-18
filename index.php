e096993cfa4b8edbdbd2ba1dbcfe04c4fd22a0b4
unknown <dev.lucassilva@gmail.com>
2015-08-18 14:08:32 -0300

    Slide responsivo feito em vídeo aula

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+<html lang="pt-BR">
+	<head>
+		<meta charset=UTF-8>
+		<title>Slide Workana</title>
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/efeito.js"></script>
+		<link href="css/style.css" rel="stylesheet" type="text/css"/>
+	</head>
+
+	<body>
+		<div id="slider">
+			<a href="#" id="prev"></a>
+			<a href="#" id="next"></a>
+
+			<ul>
+				<li class="one" style="background-image:url(slides/1.jpg);">
+					<div class="row"><h1>Confira nossas promoções</h1></div>
+					<div class="row"><p>Com nossos serviços sua empresa poderá florescer no mercado de trabalho</p></div>
+					<div class="row"><a href="#" class="botao">Confira</a></div>
+				</li>
+				<li class="two" style="background-image:url(slides/2.jpg);">
+					<div class="row"><h1>Outra chamada aqui</h1></div>
+					<div class="row"><p>Aqui vai um outro texto sobre alguma coisa importante</p></div>
+					<div class="row"><a href="#" class="botao">Confira Denovo</a></div>
+				</li>
+				<li class="three" style="background-image:url(slides/3.jpg);">
+					<div class="row"><h1>Ultima chamada</h1></div>
+					<div class="row"><p>Esse é o texto da ultima chamada</p></div>
+					<div class="row">
+						<a href="#" class="botao">Confira 1</a>
+						<a href="#" class="botao">Confira 2</a>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</body>
+</html>
